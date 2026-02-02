71313f9233bb25ffe7fcfd3013a75ed1649e158c
Stephen Mungai <mungaistephen066@gmail.com>
2026-02-02 17:31:13 +0300

    first commit

---
+++ sql/00_setup/1.sql
@@ -0,0 +1,45 @@
+--1 Select everything from test_health
+SELECT * FROM test_health;
+
+-- 2️ Select only status column
+SELECT status FROM test_health;
+
+-- 3️ Add a simple expression
+SELECT status, id*10 AS id_times_10 FROM test_health;
+
+-- 4️ Filter using WHERE
+SELECT * FROM test_health WHERE status = 'OK';
+
+--5  filter using  NOT
+SELECT * FROM test_health WHERE  status <> 'FAIL';
+
+--6 filter using  LIKE 
+SELECT * FROM test_health WHERE status LIKE 'U%';
+
+--7 order  bt created_at  desceding 
+
+SELECT * FROM  test_health ORDER BY created_at ASC;
+
+--8 limit  results 
+SELECT * FROM test_health  ORDER BY  created_at DESC LIMIT 2;
+
+--9  handle all nulls
+ALTER TABLE  test_health  ADD  COLUMN comment TEXT;
+INSERT  INTO  test_health(status) VALUES ('PENDING');
+
+
+SELECT * , COALESCE(comment, 'No comment') AS comment_text FROM test_health;
+
+--10  using aliases 
+
+SELECT status AS current_status, created_at AS timestamp  FROM test_health;
+
+
+--mu;tiple condition s
+
+SELECT * FROM test_health WHERE  status IN  ('OK', 'UNKNOWN');
+
+-- Range checks (dates, numbers)
+SELECT *
+FROM test_health
+WHERE created_at >= NOW() - INTERVAL '1 day';
